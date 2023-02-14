fc0d2910242ad3eb9f056398b86d7093386d07d6
Jerry Okolo <jerrycreative@live.co.uk>
2023-02-14 14:01:16 +0100

    fix JSON value bug

--- src/metadata.ts
+++ src/metadata.ts
@@ -1,23 +1,40 @@
-import { json, Bytes, dataSource } from "@graphprotocol/graph-ts";
-import { MetadataOutput } from "../generated/schema";
+import {
+	json,
+	log,
+	Bytes,
+	dataSource,
+	JSONValueKind,
+} from '@graphprotocol/graph-ts'
+import { MetadataOutput } from '../generated/schema'
 
 export function handleMetadata(content: Bytes): void {
-  let metadata = new MetadataOutput(dataSource.stringParam());
-  const value = json.fromBytes(content).toObject();
+	let metadata = new MetadataOutput(dataSource.stringParam())
+	const value = json.try_fromBytes(content)
 
-  if (value) {
-    const image = value.get("image");
-    const name = value.get("name");
-    const description = value.get("description");
-    const content = value.get("content");
+	if (!value.isError) {
+		let v = value.value.toObject()
+		log.warning('Postion A - found', [])
+		const image = v.get('image')
 
-    if (name && image && description && content) {
-      metadata.name = name.toString();
-      metadata.image = image.toString();
-      metadata.description = description.toString();
-      metadata.content = content.toString();
-    }
-
-    metadata.save();
-  }
+		const name = v.get('name')
+		const description = v.get('description')
+		const content = v.get('content')
+		if (name != null && !name.isNull()) {
+			metadata.name = name.toString()
+			log.warning('Postion B - name found: {}', [name.toString()])
+		}
+		if (image != null && !image.isNull()) {
+			metadata.image = image.toString()
+			log.warning('Postion C - image found: {}', [image.toString()])
+		}
+		if (description != null && !description.isNull()) {
+			metadata.description = description.toString()
+			log.warning('Postion D - description found: {}', [description.toString()])
+		}
+		if (content != null && !content.isNull()) {
+			metadata.content = content.toString()
+			log.warning('Postion E - content found: {}', [content.toString()])
+		}
+		metadata.save()
+	}
 }
